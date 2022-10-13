176bef65867d028c1d97e28f4c932379ac14da96
M Abrar Khalid <abrarprince471@gmail.com>
2022-10-14 01:27:55 +0500

    404 page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <section className="header">
+        <a href="/" className="header__logo">
+          Logo
+        </a>
+        <div className="header__nav">
+          <a href="/">Home</a>
+          <a href="/About">About</a>
+          <a href="/Home">Contact</a>
+          <a href="/400">Join Now</a>
+        </div>
+        <button href="#" className="Header__button">
+          Get In Touch
+        </button>
+      </section>
+    </>
+  );
 }
